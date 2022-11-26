bca04c6019fe16e7ea6caab4d92ba80f79e0e34a
daria-zubritska <77271382+daria-zubritska@users.noreply.github.com>
2022-11-26 12:51:51 +0200

    more useful stuff on db side ( ˙꒳​˙ )

--- cpp-atm/CardDao.cpp
+++ cpp-atm/CardDao.cpp
@@ -4,6 +4,7 @@
 #include "CardDao.h"
 #include "TransactionDao.h"
 #include "Transaction.h"
+#include <list>
 
 using namespace std;
 
@@ -70,4 +71,55 @@ DebitCard CardDao::getByNumberD(const string& number, TransactionDao tdao)
 		}
 	}
 	return DebitCard();
+}
+
+list<CreditCard> CardDao::getAllByUserC(const string& phone, TransactionDao tdao)
+{
+	list<CreditCard> newList = list<CreditCard>();
+
+	sqlite3_stmt* stmt;
+
+	sqlite3_prepare_v2(_db->db(), "SELECT * FROM card WHERE owner = ?;", -1, &stmt, NULL);
+	sqlite3_bind_text(stmt, 1, phone.c_str(), -1, SQLITE_STATIC);
+
+	while (sqlite3_step(stmt) != SQLITE_DONE) {
+
+		if (sqlite3_column_double(stmt, 6)) {
+			newList.push_back(instanceC(
+				reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0)),
+				reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)),
+				reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2)),
+				sqlite3_column_int(stmt, 3),
+				tdao.getBalance(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0))),
+				reinterpret_cast<const char*>(sqlite3_column_text(stmt, 4)),
+				(sqlite3_column_int(stmt, 5) == 1 ? true : false),
+				sqlite3_column_double(stmt, 6)));
+		}
+	}
+
+	return newList;
+}
+
+list<DebitCard> CardDao::getAllByUserD(const string& phone, TransactionDao tdao)
+{
+	list<DebitCard> newList = list<DebitCard>();
+	
+	sqlite3_stmt* stmt;
+
+	sqlite3_prepare_v2(_db->db(), "SELECT * FROM card WHERE number = ?;", -1, &stmt, NULL);
+	sqlite3_bind_text(stmt, 1, phone.c_str(), -1, SQLITE_STATIC);
+
+	while (sqlite3_step(stmt) != SQLITE_DONE) {
+
+		if (!sqlite3_column_double(stmt, 6)) {
+			newList.push_back(instanceD(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0)),
+				reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)),
+				reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2)),
+				sqlite3_column_int(stmt, 3),
+				tdao.getBalance(reinterpret_cast<const char*>(sqlite3_column_text(stmt, 0))),
+				reinterpret_cast<const char*>(sqlite3_column_text(stmt, 4)),
+				(sqlite3_column_int(stmt, 5) == 1 ? true : false)));
+		}
+	}
+	return newList;
 }
--- cpp-atm/TransactionDao.cpp
+++ cpp-atm/TransactionDao.cpp
@@ -2,6 +2,7 @@
 #include "sqlite/sqlite3.h"
 #include "DBController.h"
 #include "TransactionDao.h"
+#include <list>
 
 using namespace std;
 
@@ -58,4 +59,49 @@ double TransactionDao::getBalance(const string& number)
 	}
 
 	return balance;
+}
+
+list<Transaction> TransactionDao::getAllByCard(const string& number)
+{
+	list<Transaction> newList = list<Transaction>();
+
+	sqlite3_stmt* stmt;
+
+	sqlite3_prepare_v2(_db->db(), "SELECT * FROM transactions WHERE card_from = ?;", -1, &stmt, NULL);
+	sqlite3_bind_text(stmt, 1, number.c_str(), -1, SQLITE_STATIC);
+
+	while (sqlite3_step(stmt) != SQLITE_DONE) {
+
+		newList.push_back(instance(
+			reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)),
+			reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2)),
+			sqlite3_column_double(stmt, 3),
+			reinterpret_cast<const char*>(sqlite3_column_text(stmt, 4))));
+	}
+
+	sqlite3_prepare_v2(_db->db(), "SELECT * FROM transactions WHERE card_to = ?;", -1, &stmt, NULL);
+	sqlite3_bind_text(stmt, 1, number.c_str(), -1, SQLITE_STATIC);
+
+	while (sqlite3_step(stmt) != SQLITE_DONE) {
+
+		newList.push_back(instance(
+			reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1)),
+			reinterpret_cast<const char*>(sqlite3_column_text(stmt, 2)),
+			sqlite3_column_double(stmt, 3),
+			reinterpret_cast<const char*>(sqlite3_column_text(stmt, 4))));
+	}
+	return newList;
+}
+
+bool TransactionDao::insertTrans(Transaction& t)
+{
+	sqlite3_stmt* stmt;
+
+	sqlite3_prepare_v2(_db->db(), "INSERT INTO transactions(card_from, card_to, sum_change, date_time) VALUES (?, ?, ?, ?);", -1, &stmt, NULL);
+	sqlite3_bind_text(stmt, 1, t.getFrom().c_str(), -1, SQLITE_STATIC);
+	sqlite3_bind_text(stmt, 2, t.getTo().c_str(), -1, SQLITE_STATIC);
+	sqlite3_bind_double(stmt, 3, t.getSum());
+	sqlite3_bind_text(stmt, 4, t.getDatetime().c_str(), -1, SQLITE_STATIC);
+
+	return sqlite3_step(stmt) == SQLITE_DONE;
 }
