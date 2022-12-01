32852756a79a09c3cea56d9dfaf3a31d417790aa
Opadfnezig <shlopka18@gmail.com>
2022-12-01 04:05:49 +0200

    added PinInput interface

---
+++ cpp-atm/PinInput.h
@@ -0,0 +1,24 @@
+#include "Model.h"
+
+
+class PinInput
+{
+private:
+	Model model;
+
+public:
+	
+	PinInput(Model model)
+	{
+		this->model = model;
+	}
+	void update(int numbers)
+	{
+		
+	}
+
+	std::string excecute(std::string text)
+	{
+			
+	}
+};
